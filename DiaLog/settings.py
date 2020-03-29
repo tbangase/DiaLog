3096ab56e3dbc2597cb5c9a8ef2dbe74fc0d8213
TosHiki <ichi.toshi217@gmail.com>
2020-03-29 13:25:26 +0900

    apply bootstrap4

--- DiaLog/settings.py
+++ DiaLog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins':[
+                'bootstrap4.templatetags.bootstrap4', #Add for bootstrap
             ],
         },
     },
