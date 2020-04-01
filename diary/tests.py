626677cd95d98ce1089122b0cfa8f194ceddaa43
TosHiki <ichi.toshi217@gmail.com>
2020-04-01 14:14:29 +0900

    squashed migration file

--- diary/tests.py
+++ diary/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.utils import timezone
 
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import User, Diary
 # Create your tests here.
+
+
+class DiaryHomeViewTests(TestCase):
+  def setup(self):
+    user = User.objects.create(
+      name = "example", created_at=timezone.now(),
+      updated_at=timezone.now()
+    )
+    user.save()
+
+  def page_presencei_true(self):
+    """
+    Home page can response.
+    """
+    response = self.client.get(reverse('diary:home'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "DiaLog")
+
+
