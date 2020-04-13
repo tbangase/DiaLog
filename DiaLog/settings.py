d4ebdc4c5c71a750e2281a5ba7aadf2a07972dfd
TosHiki <ichi.toshi217@gmail.com>
2020-04-13 18:25:16 +0900

    aggregate static file for product mode

--- DiaLog/settings.py
+++ DiaLog/settings.py
@@ -148,5 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# For Custom UserModel
 AUTH_USER_MODEL = 'diary.User'
